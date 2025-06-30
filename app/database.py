e445c1902ae43b4963f2413ea53e5ab29abffbc0
Heba Allah Hashim <hebaallah.hashim68@gmail.com>
2025-06-30 22:11:25 +0300

    Database Initialized

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, create_engine, Session
+
+sqlite_url = "sqlite:///./tasks.db"
+engine = create_engine(sqlite_url, echo=True)
+
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
